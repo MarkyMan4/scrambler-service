4bd996014bfc0b9a81a46a3c5fff5049a58ac0aa
MarkyMan4 <markus.hutnik@gmail.com>
2022-12-10 01:37:27 -0600

    dependencies for web scraping lambda

--- lambda_src/data_collection/main.py
+++ lambda_src/data_collection/main.py
@@ -1,2 +1,45 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_puzzle_page(puzzle_id: str) -> dict:
+    word_data = {}
+    res = requests.get(f'https://www.sbsolver.com/s/{puzzle_id}')
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    # get data from web page
+    center_letter, outer_letters = scrape_letters(soup)
+    word_data['centerLetter'] = center_letter
+    word_data['outerLetters'] = outer_letters
+    word_data['maxScore'] = scrape_max_score(soup)
+    word_data['words'] = scrape_words(soup)
+
+    return word_data
+
+def scrape_letters(soup: BeautifulSoup) -> tuple[str, list[str]]:
+    letters_element = soup.find('input', {'placeholder': '7 unique letters'})
+    letters = letters_element['value'].lower()
+
+    return (letters[0], list(letters[1:]))
+
+def scrape_max_score(soup: BeautifulSoup) -> int:
+    score_element = soup.find('a', {'title': 'click for rank listing'})
+    
+    return int(score_element.text.split(' ')[0])
+
+def scrape_words(soup: BeautifulSoup) -> list[str]:
+    table = soup.find('table', {'class': 'bee-set'})
+    records = table.find_all('td', {'class': 'bee-hover'})
+
+    return [word.text.lower() for word in records]
+
 def lambda_handler(event=None, context=None):
-    return event
+    puzzle_id = '1676'
+    word_data = scrape_puzzle_page(puzzle_id)
+
+    # write to file
+    # with open(f'{puzzle_id}.json', 'w') as output:
+    #     json.dump(word_data, output)
+
+    return json.dumps(word_data)
--- main.tf
+++ main.tf
@@ -55,10 +55,31 @@ data "archive_file" "zip_data_collection" {
 }
 
 resource "aws_lambda_function" "data_collection_func" {
-  filename      = "${path.module}/lambda_src/data_collection/collection.zip"
-  function_name = "unscramble_data_collection"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "main.lambda_handler"
-  runtime       = "python3.8"
-  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename         = "${path.module}/lambda_src/data_collection/collection.zip"
+  function_name    = "unscramble_data_collection"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "main.lambda_handler"
+  runtime          = "python3.9"
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  source_code_hash = filebase64sha256("${path.module}/lambda_src/data_collection/collection.zip")
+  timeout          = 60
+
+  layers = [
+    aws_lambda_layer_version.requests_lambda_layer.arn,
+    aws_lambda_layer_version.bs4_lambda_layer.arn,
+  ]
+}
+
+resource "aws_lambda_layer_version" "requests_lambda_layer" {
+  filename   = "${path.module}/lambda_layers/requests.zip"
+  layer_name = "requests"
+
+  compatible_runtimes = ["python3.9"]
+}
+
+resource "aws_lambda_layer_version" "bs4_lambda_layer" {
+  filename   = "${path.module}/lambda_layers/beautifulsoup4.zip"
+  layer_name = "beautifulsoup4"
+
+  compatible_runtimes = ["python3.9"]
 }
