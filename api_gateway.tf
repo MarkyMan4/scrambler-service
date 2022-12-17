77a0ca53852c534303ddc1a0cb68c762dff48dc0
MarkyMan4 <markus.hutnik@gmail.com>
2022-12-17 11:19:57 -0600

    api gateway for new lambda

--- api_gateway.tf
+++ api_gateway.tf
@@ -4,27 +4,20 @@ resource "aws_api_gateway_rest_api" "puzzle_retriever" {
   description = "API for getting puzzle information"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+#######################################################
+# puzzle retriever
+#######################################################
+resource "aws_api_gateway_resource" "puzzle_retriever_proxy" {
   rest_api_id = aws_api_gateway_rest_api.puzzle_retriever.id
   parent_id   = aws_api_gateway_rest_api.puzzle_retriever.root_resource_id
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "puzzle_retriever_proxy" {
   rest_api_id   = aws_api_gateway_rest_api.puzzle_retriever.id
-  resource_id   = aws_api_gateway_resource.proxy.id
+  resource_id   = aws_api_gateway_resource.puzzle_retriever_proxy.id
   http_method   = "ANY"
   authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "puzzle_retriever_lambda" {
-  rest_api_id = aws_api_gateway_rest_api.puzzle_retriever.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.puzzle_retriever_func.invoke_arn
 }
 
 # proxy resource can't match empty path at root of api, so need to configure same 
@@ -34,6 +27,16 @@ resource "aws_api_gateway_method" "proxy_root" {
   resource_id   = aws_api_gateway_rest_api.puzzle_retriever.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "puzzle_retriever_lambda" {
+  rest_api_id = aws_api_gateway_rest_api.puzzle_retriever.id
+  resource_id = aws_api_gateway_method.puzzle_retriever_proxy.resource_id
+  http_method = aws_api_gateway_method.puzzle_retriever_proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.puzzle_retriever_func.invoke_arn
 }
 
 resource "aws_api_gateway_integration" "puzzle_retriever_lambda_root" {
@@ -46,19 +49,8 @@ resource "aws_api_gateway_integration" "puzzle_retriever_lambda_root" {
   uri                     = aws_lambda_function.puzzle_retriever_func.invoke_arn
 }
 
-#
-resource "aws_api_gateway_deployment" "scrambler_api_deployment" {
-  depends_on = [
-    aws_api_gateway_integration.puzzle_retriever_lambda,
-    aws_api_gateway_integration.puzzle_retriever_lambda_root,
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.puzzle_retriever.id
-  stage_name  = "puzzle_retriever"
-}
-
 # allow api gateway to invoke lambda
-resource "aws_lambda_permission" "apigw" {
+resource "aws_lambda_permission" "allow_invoke_puzzle_retriever" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.puzzle_retriever_func.function_name
@@ -67,4 +59,69 @@ resource "aws_lambda_permission" "apigw" {
   # The /*/* portion grants access from any method on any resource
   # within the API Gateway "REST API".
   source_arn = "${aws_api_gateway_rest_api.puzzle_retriever.execution_arn}/*/*"
+}
+
+
+#######################################################
+# ID lister
+#######################################################
+resource "aws_api_gateway_resource" "list_puzzle_ids_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.puzzle_retriever.id
+  parent_id   = aws_api_gateway_rest_api.puzzle_retriever.root_resource_id
+  path_part   = "list_puzzles"
+}
+
+resource "aws_api_gateway_method" "list_puzzle_ids_proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.puzzle_retriever.id
+  resource_id   = aws_api_gateway_resource.list_puzzle_ids_proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "list_puzzle_ids_lambda" {
+  rest_api_id = aws_api_gateway_rest_api.puzzle_retriever.id
+  resource_id = aws_api_gateway_method.list_puzzle_ids_proxy.resource_id
+  http_method = aws_api_gateway_method.list_puzzle_ids_proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.list_puzzle_ids_func.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "list_puzzle_ids_lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.puzzle_retriever.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.list_puzzle_ids_func.invoke_arn
+}
+
+# allow api gateway to invoke lambda
+resource "aws_lambda_permission" "allow_invoke_list_puzzle_ids" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.list_puzzle_ids_func.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.puzzle_retriever.execution_arn}/*/*"
+}
+
+
+#######################################################
+# deployment
+#######################################################
+resource "aws_api_gateway_deployment" "scrambler_api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.puzzle_retriever_lambda,
+    aws_api_gateway_integration.puzzle_retriever_lambda_root,
+    aws_api_gateway_integration.list_puzzle_ids_lambda,
+    aws_api_gateway_integration.list_puzzle_ids_lambda_root
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.puzzle_retriever.id
+  stage_name  = "puzzle_retriever"
 }
